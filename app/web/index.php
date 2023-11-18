806a771b8e374022d719cac0267419727c5cbf63
Dmitriy Krivopalov <dimarik82@gmail.com>
2023-11-18 09:57:17 +0300

    initialization

--- app/web/index.php
+++ app/web/index.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config/bootstrap.php';
-require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/api.php';
 
